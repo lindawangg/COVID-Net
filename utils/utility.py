754a8570843cb36d8128cfcde1dc69f9c284b632
habout <hossain.aboutalebi@gmail.com>
2021-05-01 15:22:56 -0400

    Added the resnet type selector

--- utils/utility.py
+++ utils/utility.py
@@ -2,7 +2,7 @@ from model.resnet1 import ResNet50_1
 from model.resnet2 import ResNet50_2
 
 def select_resnet_type(name,classes,model_semantic):
-    if(name is "resnet1"):
+    if(name == "resnet1"):
         return ResNet50_1(classes=classes, model_semantic=model_semantic)
-    elif(name is "resnet2"):
+    elif(name == "resnet2"):
         return ResNet50_2(classes=classes, model_semantic=model_semantic)
